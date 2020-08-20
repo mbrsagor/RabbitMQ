1653e61748886170ebb95f9dcee7973725d1081a
Sagor <mbrsagor@gmail.com>
2020-08-21 00:43:47 +0600

    Time complexity using loop

---
+++ time-complexity/demo2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, result = 10;
+    // scanf("%d", &n);
+    result = 0;
+
+    for (i = 0; i <= n; i++) {
+        result += 1;
+    }
+
+    printf("Result : %d\n", result);
+
+    return 0;
+}
