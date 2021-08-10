41bad8d6d170404852575612ffcbef19f3cea141
mbrsagor <mbrsagor@gmail.com>
2021-08-10 21:10:07 +0600

    New: Single link list

---
+++ ssl.py
@@ -0,0 +1,11 @@
+class Node(object):
+    def __init__(self, data):
+        self.item = data
+        self.ref = None
+
+
+class LinkList(object):
+    def __str__(self):
+        self.start_node = None
+
+# https://stackabuse.com/linked-lists-in-detail-with-python-examples-single-linked-lists
