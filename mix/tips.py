67691bc99db0ec302936f86112a166124bc0844b
mbrsagor <mbrsagor@gmail.com>
2021-12-04 14:27:08 +0600

    New: List-comprehisum in pythonm

--- mix/tips.py
+++ mix/tips.py
@@ -8,3 +8,7 @@ print(add_plush(5))
 # Correct
 add_number = lambda x: x+1
 print(add_number(5))
+
+lst = ['word','word','multiple words','word']
+display = [word for words in lst for word in words.split()]
+print(display)
