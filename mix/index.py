dddcbad4227409579a39f7f15ab3b285215eb3a9
mbrsagor <mbrsagor@gmail.com>
2021-12-23 16:39:07 +0600

    New: Find square number

--- mix/index.py
+++ mix/index.py
@@ -12,60 +12,51 @@ my_function(fname = "Tobias", lname = "Refsnes")
 """
 
 
-
 class Rectangle(object):
-  def __init__(self, length, width):
-    self.length = length
-    self.width = width
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-  def area(self):
-    return self.length * self.width
+    def area(self):
+        return self.length * self.width
 
-  def perimeter(self):
-    return 2 * self.length + 2 * self.width
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
+
 
 class RectangleV2(Rectangle):
-  pass
+    pass
+
 
 v2 = RectangleV2(20, 50)
 print(v2.perimeter())
 
 
 class Square:
-  def __init__(self, length):
-    self.length = length
+    def __init__(self, length):
+        self.length = length
 
-  def area(self):
-    return self.length * self.length
+    def area(self):
+        return self.length * self.length
 
-  def perimeter(self):
-    return 4 * self.length
+    def perimeter(self):
+        return 4 * self.length
 
 
 square = Square(4)
 print(square.area())
-rectangle = Rectangle(2,4)
+rectangle = Rectangle(2, 4)
 print(rectangle.area())
 
 
-class Person(object):
+def get_square_numbers(numbers):
+    square_numbers = []
+    for number in numbers:
+        square_numbers.append(number * number)
 
-  
-  def display():
-    print("I'm calling from property method")
-
-
-Person.display()
-
-countryCode = {
-  "code1": "Bangladesh",
-  "code2": "India",
-  "code3": "Japan"
-}
+    return square_numbers
 
 
-def find_country(code):
-  return countryCode[code]
-
-name = find_country('code1')
-print(name)
+_numbers = [2, 4, 8, 9]
+display = get_square_numbers(_numbers)
+print(display)
