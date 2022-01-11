5f1610f6ce07ef056a11684cd1a8cea748561df7
mbrsagor <mbrsagor@gmail.com>
2022-01-11 20:24:34 +0600

    New: Find leap year

---
+++ mix/leap_year.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    leap = False
+
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+
+    return leap
+
+
+year = 2032
+print(is_leap(year))
